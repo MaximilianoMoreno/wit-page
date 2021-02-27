6c99e31d006056c7823132537ddeaebca99477d7
Maximiliano Moreno <maximiliano.moreno@globallogic.com>
2021-02-27 10:32:09 -0300

    cambios landing horarios

--- js/horarios.js
+++ js/horarios.js
@@ -28,10 +28,35 @@
  */
 
 const CURSOS = {
+  adults1: {
+    nombreCurso: "Adults 1",
+    diaCursado: "Lunes y Miércoles de 10 a 11.30",
+    urlIframe: "https://view.genial.ly/60245475cd123c0d1ce7ebf2",
+  },
+  adults2: {
+    nombreCurso: "Adults 2",
+    diaCursado: "Lunes y Miércoles de 20.30 a 22",
+    urlIframe: "https://view.genial.ly/60245475cd123c0d1ce7ebf2",
+  },
   adults3: {
     nombreCurso: "Adults 3",
     diaCursado: "Lunes y Miércoles de 10 a 11.30",
     urlIframe: "https://view.genial.ly/60245475cd123c0d1ce7ebf2",
+  },
+  adults4: {
+    nombreCurso: "Adults 4",
+    diaCursado: "Martes y Jueves de 20.30 a 22",
+    urlIframe: "https://view.genial.ly/602325813cb0490d27f878fa",
+  },
+  adults5: {
+    nombreCurso: "Adults 5",
+    diaCursado: "Martes y Jueves de 20.30 a 22",
+    urlIframe: "https://view.genial.ly/602325813cb0490d27f878fa",
+  },
+  perfecting: {
+    nombreCurso: "Perfecting",
+    diaCursado: "Martes y Jueves de 20.30 a 22",
+    urlIframe: "https://view.genial.ly/602325813cb0490d27f878fa",
   },
   kids1: {
     nombreCurso: "Kids 1",
@@ -40,6 +65,41 @@ const CURSOS = {
   },
   bigkids1: {
     nombreCurso: "Big Kids 1",
+    diaCursado: "Lunes y Miércoles de 18.45 a 20.15",
+    urlIframe: "https://view.genial.ly/60245475cd123c0d1ce7ebf2",
+  },
+  kids1mm: {
+    nombreCurso: "Kids 1",
+    diaCursado: "Martes y Jueves de 10 a 11.30",
+    urlIframe: "https://view.genial.ly/602325813cb0490d27f878fa",
+  },
+  kids1mt: {
+    nombreCurso: "Kids 1",
+    diaCursado: "Martes y Jueves de 18.45 a 20.15",
+    urlIframe: "https://view.genial.ly/602325813cb0490d27f878fa",
+  },
+  kids2: {
+    nombreCurso: "Kids 2",
+    diaCursado: "Martes y Jueves de 18.45 a 20.15",
+    urlIframe: "https://view.genial.ly/602325813cb0490d27f878fa",
+  },
+  bigkids1: {
+    nombreCurso: "Big Kids 1",
+    diaCursado: "Lunes y Miércoles de 18.45 a 20.15",
+    urlIframe: "https://view.genial.ly/602325813cb0490d27f878fa",
+  },
+  youngsters1mt: {
+    nombreCurso: "Youngsters 1",
+    diaCursado: "Martes y Jueves de 17 a 18.30",
+    urlIframe: "https://view.genial.ly/60245475cd123c0d1ce7ebf2",
+  },
+  youngsters1: {
+    nombreCurso: "Youngsters 1",
+    diaCursado: "Lunes y Miércoles de 18.45 a 20.15",
+    urlIframe: "https://view.genial.ly/60245475cd123c0d1ce7ebf2",
+  },
+  youngsters2: {
+    nombreCurso: "Youngsters 2",
     diaCursado: "Lunes y Miércoles de 18.45 a 20.15",
     urlIframe: "https://view.genial.ly/60245475cd123c0d1ce7ebf2",
   },
@@ -53,10 +113,13 @@ function init() {
 function buscarCurso() {
   let nombreCurso = obtenerValorParametroURL("curso");
   let curso = CURSOS[nombreCurso];
-
-  document.getElementById("nombre-curso").innerText = curso.nombreCurso;
-  document.getElementById("dia-cursado").innerText = curso.diaCursado;
-  document.getElementById("url-iframe").src = curso.urlIframe;
+  if (curso) {
+    document.getElementById("nombre-curso").innerText = curso.nombreCurso;
+    document.getElementById("dia-cursado").innerText = curso.diaCursado;
+    document.getElementById("url-iframe").src = curso.urlIframe;
+  } else {
+    document.getElementById("overlay").style.display = "block";
+  }
 }
 
 function obtenerValorParametroURL(nombreParam) {
